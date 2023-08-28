a4868b67d5374ab34c6c2af78d727ef0d71af9b5
CyrusNchege <cyruschegecc@gmail.com>
2023-08-28 12:25:00 +0300

    Refactor: Change of direcory arrangement of the project

--- taskapis/asgi.py
+++ taskapis/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for taskapi project.
+ASGI config for taskapis project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskapi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskapis.settings')
 
 application = get_asgi_application()
